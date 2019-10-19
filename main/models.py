ce89910c6e075193f9d67727c0bae408cd8713ee
Nyota254 <knyota66@gmail.com>
2019-10-19 18:58:51 +0300

    add models for site upload and rating

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    '''
+    This model will contain the fields for the user uploaded projects
+    '''
+    title = models.CharField(max_length=40)
+    description = models.TextField()
+    link = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='site_photos/')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+class Rating(models.Model):
+    '''
+    This model will contain the ratings for diffrent categories
+    '''
+    design = models.IntegerField(choices=[(i,i) for i in range(1,11)])
+    usability = models.IntegerField(choices=[(i,i) for i in range(1,11)])
+    content = models.IntegerField(choices=[(i,i) for i in range(1,11)])
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE) 
+
